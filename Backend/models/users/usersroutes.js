063748350186b46ac8e63af9864516e8c47c8fc1
Jatin <jatinrajvani.cg@gmail.com>
2025-10-09 11:52:51 +0530

    Adding a api for updating the user

--- Backend/models/users/usersroutes.js
+++ Backend/models/users/usersroutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { addFullUser, addEmergencyGuest} = require('./usersControllers');
+const { addFullUser, addEmergencyGuest , completeGuestProfile} = require('./usersControllers');
 
 // Full user signup
 router.post('/add', addFullUser);
 
 // Emergency guest signup
 router.post('/emergency', addEmergencyGuest);
+
+router.patch('/update/:id', completeGuestProfile);
 
 module.exports = router;
